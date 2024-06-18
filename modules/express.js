c95ba8b1ab8971420bfe3cbb8f04860ed50361de
Gustavo Mello <oliver7mello@gmail.com>
2024-06-18 01:59:05 -0300

    Conectando com banco de dados, e solicitando dados

--- modules/express.js
+++ modules/express.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const UserModel = require("../src/models/user.model");
+
+app.use(express.json());
 
 app.get("/home", (req, res) => {
-  res.contentType("text/html");
-  res.status(200).send("<h1>Olá Mundo!</h1>");
+  res.status(200).send('<h1 style="color: green">Olá Mundo</h1>');
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Jhon Doe",
-      email: "jhon@doe.com",
-    },
-    {
-      name: "Jane Doe",
-      email: "jane.doe@gmail.com",
-    },
-  ];
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-  res.contentType("application/json");
-  res.status(200).json(users);
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    send.status(500).send(error.message);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Rodando com express na porta ${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
