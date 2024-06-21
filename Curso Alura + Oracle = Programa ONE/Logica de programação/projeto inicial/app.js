c5ff57b7bd76b30cec33ad2fbc0f2a2c28d2b0b9
Gustavo Mello <oliver7mello@gmail.com>
2024-06-21 13:58:51 -0300

    Aula 1.5

--- Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/app.js
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/app.js
@@ -1,1 +1,12 @@
-alert("Bem vindo ao jogo do número secreto");
+let mensagemDeBoasVindas = "Boas vindas ao App";
+alert("mensagemDeBoasVindas");
+
+let numeroSecreto = 5;
+
+let chute = prompt("Escolha entre 1 e 10:");
+
+if (numeroSecreto == chute) {
+  alert("Isso aí! Você descobriu o número secreto.");
+} else {
+  alert("Essa não, Você errou o numero secreto.");
+}
---
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/test.js
@@ -0,0 +1,19 @@
+alert("Boas-vindas ao nosso site");
+
+let nome = "Lua";
+
+let idade = 25;
+
+numeroDeVendas = 50;
+
+saldoDisponivel = 1000;
+
+alert("Erro!");
+
+let mensagemDeErro = "Erro! preencha todos os campos";
+
+alert(mensagemDeErro);
+
+let name = prompt("Digite seu nome:");
+
+let age = prompt("Digite sua idade:");
