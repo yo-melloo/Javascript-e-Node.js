7aa8dcb473b1bc26f7aa2553ef2c8a1f719d5a10
Gustavo Mello <oliver7mello@gmail.com>
2024-06-21 09:24:42 -0300

    Update index.js

--- Ambiente de Testes NODE/Curso DevSoutinho/async e await/index.js
+++ Ambiente de Testes NODE/Curso DevSoutinho/async e await/index.js
@@ -1,5 +1,7 @@
+const user = "yo-melloo";
+
 async function pegaDados() {
-  const a = await fetch("https://api.github.com/users/yo-melloo");
+  const a = await fetch(`https://api.github.com/users/${user}`);
   const resultadoConvertido = await a.json();
   //console.log("resultado: ", a);
   console.log(resultadoConvertido);
