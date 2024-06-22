1434b8575663df49cbcc2f066d2a0cf1314764d6
Gustavo Mello <oliver7mello@gmail.com>
2024-06-22 13:22:59 -0300

    desafios 3 e 4.

--- Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/app.js
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/app.js
@@ -1,16 +1,30 @@
-let mensagemDeBoasVindas = "Boas vindas ao App";
-alert(mensagemDeBoasVindas);
-
-let numeroSecreto = 4;
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-let chute = prompt("Escolha entre 1 e 10:");
-
-if (numeroSecreto == chute) {
-  alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}).`);
-} else {
-  alert("Essa não, Você errou o numero secreto.");
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 1 e 10");
+  // se chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    // tentativas = tentativas + 1
+    tentativas++;
+  }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
+);
 
 /* 
 É importante lembrar que, quando o projeto ficar disponível para todas as pessoas,
