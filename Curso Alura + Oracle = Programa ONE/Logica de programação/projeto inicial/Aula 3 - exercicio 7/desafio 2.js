fd766378f82d55123984a8cc4464682ef485ec4a
Gustavo Mello <oliver7mello@gmail.com>
2024-06-22 11:41:13 -0300

    Desafio 2 - Exercicio 7 - Aula 3

---
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto inicial/Aula 3 - exercicio 7/desafio 2.js
@@ -0,0 +1,8 @@
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+let contador = 10;
+
+while (contador >= 0) {
+  console.log(`Contador: ${contador}`);
+  contador--;
+}
