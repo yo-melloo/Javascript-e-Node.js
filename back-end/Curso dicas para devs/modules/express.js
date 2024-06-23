80e70480d14a0515f444c42d8411744e625222a6
Gustavo Mello <oliver7mello@gmail.com>
2024-06-23 19:23:22 -0300

    arquivo comentado

--- back-end/Curso dicas para devs/modules/express.js
+++ back-end/Curso dicas para devs/modules/express.js
@@ -1,16 +1,24 @@
 /* Código comentado por mim para fins de documentação */
 
-const express = require("express"); /* Importando o Node.Express */
+/* Importando o Node.Express */
+const express = require("express");
 const app = express();
+
+/* Porta de conexão HTTP */
 const port = 8080;
+
+/* Importando Mongoose (Biblioteca de aplicação do MongoDB para Objetos) */
 const UserModel = require("../src/models/user.model");
 
+/* Declarando trabalhar com JSON (boa parte) no código*/
 app.use(express.json());
 
+/* Criando a página inicial do servidor */
 app.get("/home", (req, res) => {
   res.status(200).send('<h1 style="color: green">Olá Mundo</h1>');
 });
 
+/* Criando a página de usuários */
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
@@ -20,6 +28,7 @@ app.get("/users", async (req, res) => {
   }
 });
 
+/* Criando a página de usuário único */
 app.get("/users/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -31,6 +40,7 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+/* Criando "envio" de dados para o servidor */
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
@@ -40,6 +50,7 @@ app.post("/users", async (req, res) => {
   }
 });
 
+/* "Lançando" o servidor */
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}/home`);
 });
