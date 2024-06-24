73260baf05c23edab0093192a5a052b37e539dae
Gustavo Mello <oliver7mello@gmail.com>
2024-06-24 19:56:03 -0300

    update

---
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto explorando listas e funcoes/Desafios/desafio 1/app.js
@@ -0,0 +1,9 @@
+//Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+
+//Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
+//Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+
+//Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+
+//Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
---
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto explorando listas e funcoes/Desafios/desafio 2/desafio2.js
@@ -0,0 +1,18 @@
+//Criar uma função que exibe "Olá, mundo!" no console.
+console.log("Olá, mundo!");
+
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+let name = prompt("Digite um nome");
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+let number = Number(prompt("Digite um número"));
+console.log(number * 2);
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+let num1 = Number(prompt("Número 1"));
+let num2 = Number(prompt("Número 2"));
+let num3 = Number(prompt("Número 3"));
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
