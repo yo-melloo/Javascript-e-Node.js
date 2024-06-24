d5c7f17c0cdfc17c5c0b95bb0b3f714870ca4f0b
Gustavo Mello <oliver7mello@gmail.com>
2024-06-24 07:41:34 -0300

    Curso 2

---
+++ Curso Alura + Oracle = Programa ONE/Logica de programação/projeto explorando listas e funcoes/app.js
@@ -0,0 +1,5 @@
+let title = document.querySelector("h1");
+title.innerHTML = "Desafio do Número Secreto";
+
+let text = document.querySelector("p");
+text.innerHTML = "encontre o número escondido:";
